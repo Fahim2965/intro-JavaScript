44f2038fb69665aadf77bc14705b57de00e23fca
Fahim Muntasir <fmuntasir2@brooklynsteamcenter.org>
2023-04-17 14:52:42 -0400

    yessssss

---
+++ intro.js
@@ -0,0 +1,36 @@
+console.log("Hello")
+let myVariable = "GG"
+let myName = "Fahim"
+let myAge = 16
+
+console.log("Hello my name is " + myName + ". My age is " + myAge)
+
+if (200 == 150) {
+    console.log("Yes")
+} 
+else if (200 == 20) {
+    console.log("True")
+}
+else {
+    console.log("False")
+}
+
+let percentage = 100
+
+  if (percentage <= 100 && percentage >= 80) {
+    grades = "A";
+  } else if (percentage <= 79 && percentage >= 60) {
+    grades = "B";
+  } else if (percentage <= 59 && percentage >= 40) {
+    grades = "C";
+  } else {
+    grades = "F";
+  }
+
+
+
+let myList = [90, 100,420]
+
+for (let item of myList) {
+    console.log(item * 10)
+}
