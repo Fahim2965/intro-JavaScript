0c13b9cea4016f647bbf35eaaf2c9f27710c614d
Fahim Muntasir <fmuntasir2@brooklynsteamcenter.org>
2023-04-28 14:52:11 -0400

    qwertrew

---
+++ stop-watch.js
@@ -0,0 +1,11 @@
+function START () {
+    alert("You clicked the START button")
+}
+
+function STOP () {
+    alert("You clicked the STOP button")
+}
+
+function RESET () {
+    alert("You clicked the RESET button")
+}
