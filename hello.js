e04ce366a9238cb7acdc5d7cb6a44e240d7b4d4f
Fahim Muntasir <fmuntasir2@brooklynsteamcenter.org>
2023-05-02 13:58:31 -0400

    owieuh

---
+++ hello.js
@@ -0,0 +1,18 @@
+console.log("Hello")
+let myVariable = "GG"
+let myName = "Fahim"
+let myAge = 16
+
+console.log("Hello my name is " + myName + ". My age is " + myAge)
+
+if (200 == 150) {
+    console.log("Yes")
+} 
+else if (200 == 20) {
+    console.log("True")
+}
+else {
+    console.log("False")
+}
+
+let percentage = 100
--- stop-watch.js
+++ stop-watch.js
@@ -1,5 +1,28 @@
+let milliseconds = 0
+let seconds = 0
+let minutes = 0
+
+let digits = document.getElementById('digits')
+
+let interval;
+
 function START () {
-    alert("You clicked the START button")
+    interval = setInterval(function () {
+        milliseconds = milliseconds + 10
+
+        if (milliseconds >= 1000) {
+            seconds = seconds + 1
+            milliseconds = 0
+        }
+        
+        if (seconds >= 60) {
+            minutes += 1
+            seconds = 0
+        }
+
+        digits.innerHTML = String(minutes).padStart(2, '0') + ':' + String(seconds).padStart(2, '0') + ':' + milliseconds /
+
+    }, 10)
 }
 
 function STOP () {
